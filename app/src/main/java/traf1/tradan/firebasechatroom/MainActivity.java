212d62ea8137f11263e5960b398108ff46efe5a8
dltra <dltra@fcps.edu>
2020-01-12 21:48:31 -0500

    Add event listener and print value change to console

--- app/src/main/java/traf1/tradan/firebasechatroom/MainActivity.java
+++ app/src/main/java/traf1/tradan/firebasechatroom/MainActivity.java
@@ -1,11 +1,15 @@
 package traf1.tradan.firebasechatroom;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,5 +22,21 @@ public class MainActivity extends AppCompatActivity {
         DatabaseReference myRef = database.getReference("message");
 
         myRef.setValue("Hello, World!");
+
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                System.out.println("Firebase test: Value is: " + value);
+            }
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Failed to read value
+                System.out.println("Failed to read value."+ databaseError.toException());
+            }
+        });
     }
 }
