f6c66b6a949dec092a35f16e1b2b22eb17837e9e
dltra <dltra@fcps.edu>
2020-01-12 22:34:01 -0500

    Sign up new user by email and password

--- app/src/main/java/traf1/tradan/firebasechatroom/MainActivity.java
+++ app/src/main/java/traf1/tradan/firebasechatroom/MainActivity.java
@@ -1,10 +1,14 @@
 package traf1.tradan.firebasechatroom;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -21,6 +25,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         // Initialize Firebase Auth
         mAuth = FirebaseAuth.getInstance();
+        mAuth.createUserWithEmailAndPassword("abc@abc.com", "tjhsst")
+           .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+               @Override
+               public void onComplete(@NonNull Task<AuthResult> task) {
+                   if (task.isSuccessful()) {
+                       // Sign in success, update UI with the signed-in user's information
+                       System.out.println("Authen test:  createUserWithEmail:success");
+                       FirebaseUser user = mAuth.getCurrentUser();
+                       updateUI(user);
+                   } else {
+                       // If sign in fails, display a message to the user.
+                       System.out.println("Authen test:  createUserWithEmail:failure"+task.getException());
+                       Toast.makeText(getApplicationContext(), "Authentication failed.",
+                               Toast.LENGTH_SHORT).show();
+                       updateUI(null);
+                   }
+               }
+           });
         // Write a message to the database
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference myRef = database.getReference("message");
